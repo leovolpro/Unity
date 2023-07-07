46b08ff7630e845860542359df21dd91d1d9c5b6
leovolpro <138884768+leovolpro@users.noreply.github.com>
2023-07-07 16:20:12 +0300

    Crickets&Destroy

--- Destroy.cs
+++ Destroy.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class Destroy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
-  
-     
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(" crickets");
+        
     }
 }
